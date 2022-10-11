cb4a0b2500fdac409f11b36138c271a49cb40bec
Nadezhda Maul <maulnadezda@gmail.com>
2022-10-12 00:08:58 +0300

    9

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,24 @@
-console.log('script work!');
+$(document).ready(function(){
+
+
+
+  $('.js-header-burger').on('click', function() {
+
+    $('.js-menu').slideToggle();
+  });
+
+
+  // Аккордеоны - часто задаваемые вопросы
+  let prevAccordionBtn;
+
+  $('.js-accordion-btn').on('click', function() {
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+    } else {
+      $('.js-accordion-btn').next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
+  });
+
+});

f17d9883b74a52522cb0981dbd847b5d4ca8c998
Nadezhda Maul <maulnadezda@gmail.com>
2022-10-16 18:22:04 +0300

    11

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -21,4 +21,26 @@ $(document).ready(function(){
     }
   });
 
+  // Табы в контактах
+  $('.js-tabs-link').on('click', function(event){
+    event.preventDefault();
+
+    $('.js-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    const index = $(this).index('.js-tabs-link');
+
+    $('.js-tabs-content').removeClass('active');
+    $('.js-tabs-content').eq(index).addClass('active');
+  });
+
+   // Слик слайдер
+   $('.js-reviews-carousel').slick({
+    autoplay: false,
+    infinite: false,
+    dots: true
+  });
+
+
+
 });
